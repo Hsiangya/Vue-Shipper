82291c54709e42595dffcfac77bda74f0de6b110
Hsiangya <hsiangyatang@gmail.com>
2023-02-10 00:02:29 +0800

    修改认证信息

--- src/views/account/AuthEditView.vue
+++ src/views/account/AuthEditView.vue
@@ -10,14 +10,67 @@
         </router-link>
       </div>
     </template>
-    <el-form :model=""></el-form>
+    <el-form :model="state.form" ref="FormRef" label-width="140px">
+      <div>
+        <h4>企业信息</h4>
+        <el-row :gutter="30">
+          <el-col :span="12">
+            <el-form-item style="margin-top:24px" :error="state.error.title" label="企业名称">
+              <el-input v-model="state.form.title" placeholder="企业名称"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item style="margin-top:24px" :error="state.error.unique_id" label="统一社会信用代码">
+              <el-input v-model="state.form.unique_id" placeholder="统一社会信用代码"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="30">
+          <el-col :span="12">
+            <el-form-item style="margin-top:24px;" :error="state.form.license_path" label="营业执照">
+              <ul v-if="state.form.license_path" class="el-upload-list el-upload-list--picture-card"
+                  style="width: 200px;height: 150px;">
+                <li class="el-upload-list__item is-success" style="width: 200px;height: 120px;">
+                  <img class="el-upload-list__item-thumbnail" :src="state.form.license_path_url">
+
+                  <span class="el-upload-list__item-actions">
+                    <span class="el-upload-list__item-preview" @click="state.DialogLicenceVisible=true">
+                      <el-icon><ZoomIn/></el-icon>
+                    </span>
+                    <span class="el-upload-list__item-delete" @click="RemoveLicenceImage"></span>
+                    <el-icon><Delete/></el-icon>
+                  </span>
+                </li>
+              </ul>
+              <el-upload
+                  v-else
+                  style="width: 200px;height: 150px;"
+                  drag
+                  :data="{type:'licence_path'}"
+                  :show-file-list="false"
+                  :multiple="false"
+                  :action="imageUploadUrl"
+                  :before-upload="beforeImageUpload"
+                  :on-success="uploadSuccessWrapper('licence_path')">
+                <el-icon class="el-icon--upload">
+                  <UploadFilled/>
+                </el-icon>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+
+        </el-row>
+      </div>
+    </el-form>
   </el-card>
 </template>
 
 <script setup>
 import {reactive} from "vue";
+import {UploadFilled, ZoomIn, Delete} from '@element-plus/icons-vue'
 
 const state = reactive({
+  DialogLicenceVisible: false,
   form: {
     title: "",
     unique_id: "",
@@ -45,6 +98,9 @@ const state = reactive({
   }
 })
 
+function RemoveLicenceImage() {
+
+}
 </script>
 
 <style scoped>
